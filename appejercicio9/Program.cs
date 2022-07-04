f4992e0517467f82c2f4d942ee9e9554d95ad789
pamela <pamela@AIWA_NA-1413>
2022-07-04 18:05:58 -0300

    Agregar archivos de proyecto.

---
+++ appejercicio9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace appejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primernumero, segundonumero, tercernumero, cuartonumero, suma;
+
+            Console.Write("ingrese el primer numero ");
+            primernumero= int.Parse(Console.ReadLine());
+            Console.Write("ingrese el segundo numero ");
+            segundonumero = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el tercer mumero ");
+            tercernumero = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el cuarto numero ");
+            cuartonumero = int.Parse(Console.ReadLine());
+
+            suma = primernumero + segundonumero + tercernumero + cuartonumero;
+
+            Console.WriteLine($"la suma de numeros primos es {suma}");
+
+
+            
+        }
+    }
+}
